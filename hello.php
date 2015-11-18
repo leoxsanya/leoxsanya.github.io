c74dfa3a84278ab9e7939d6afa61d40be67c3214
Sanya Chou <k80092@gmail.com>
2015-11-19 00:13:47 +0900

    naosi

--- hello.php
+++ hello.php
@@ -11,36 +11,6 @@
 <?php 
 	echo "Hello World</p>";
 ?>
-<?php
-	echo date('H:i, jS F').'</p>';
-	
-	$tireqty=$_POST['tireqty'];
-	$oilqty=$_POST['oilqty'];
-	$sparkqty=$_POST['sparkqty'];
-	$fruit = "りんご";
-	echo "\$fruitには{$fruit}が代入されています。<br/>";
-	$a=1;
-	$b=$a;
-	//$b=$a->__clone();
-	$c=&$a;
-	$a=5;
-	echo $b."<br/>";
-	
-
-?>
-
-
-<?php
-$a=1;
-$b=2;
-function test(){
-	$GLOBALS[a]=$GLOBALS[a]+$GLOBALS[b];
-}
-test();
-echo $a;		//輸出結果會是3
-
-?>
-?>
 
 </body>
 </html>
